f7b6b924c0aed4e05b90ca93e82c2fa8645259f5
Luyin Huang <luyinhuang.nathaniel@gmail.com>
2024-08-28 14:06:26 -0400

    commit1

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+	
+	@RequestMapping("api/health")
+	public String sayHello() {
+	        return "API Health is good";
+	} 
+
+}
